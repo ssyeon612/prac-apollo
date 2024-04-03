13a34109999aba05aa5cb3267207877a64e3f887
ssyeon612 <ssyeon612@gmai.com>
2024-04-03 22:44:02 +0900

    feat: edit mutaition 추가

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const typeDefs = gql`
     }
     type Mutation {
         insertEquipment(id: String, used_by: String, count: Int, new_or_used: String): Equipment
+        editEquipment(id: String, used_by: String, count: Int, new_or_used: String): Equipment
         deleteEquipment(id: String): Equipment
     }
     type Team {
@@ -53,6 +54,16 @@ const resolvers = {
         insertEquipment: (parent, args, context, info) => {
             database.equipments.push(args);
             return args;
+        },
+        editEquipment: (parent, args, context, info) => {
+            return database.equipments
+                .filter((equipment) => {
+                    return equipment.id === args.id;
+                })
+                .map((equipment) => {
+                    Object.assign(equipment, args);
+                    return equipment;
+                })[0];
         },
         deleteEquipment: (parent, args, context, info) => {
             const deleted = database.equipments.filter((equipment) => {
