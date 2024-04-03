be9da0153baf3e34b9cba8c017ec225082de6480
ssyeon612 <ssyeon612@gmai.com>
2024-04-03 21:13:45 +0900

    feat: apollo server 세팅

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const database = require("./database");
-console.log(database.people[3]);
+const { ApolloServer, gql } = require("apollo-server");
+const typeDefs = gql`
+    type Query {
+        teams: [Team]
+    }
+    type Team {
+        id: Int
+        manager: String
+        office: String
+        extension_number: String
+        mascot: String
+        cleaning_duty: String
+        project: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        teams: () => database.teams,
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
