d2d3b2edd7be7e5b971c78ac5424684f8da37d27
ssyeon612 <ssyeon612@gmai.com>
2024-04-03 21:58:27 +0900

    insert mutation 추가

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const typeDefs = gql`
         supplies: [Supply]
     }
     type Mutation {
+        insertEquipment(id: String, used_by: String, count: Int, new_or_used: String): Equipment
         deleteEquipment(id: String): Equipment
     }
     type Team {
@@ -49,6 +50,10 @@ const resolvers = {
         supplies: () => database.supplies,
     },
     Mutation: {
+        insertEquipment: (parent, args, context, info) => {
+            database.equipments.push(args);
+            return args;
+        },
         deleteEquipment: (parent, args, context, info) => {
             const deleted = database.equipments.filter((equipment) => {
                 return equipment.id === args.id;
