4bac9aa17986c2a8cf68d2010156e7c3ad762e48
ssyeon612 <ssyeon612@gmai.com>
2024-04-04 22:54:35 +0900

    feat: enum 추가

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const _ = require("lodash");
 
 const queries = require("./typedefs-resolvers/_queries");
 const mutations = require("./typedefs-resolvers/_mutations");
+const enums = require("./typedefs-resolvers/_enums");
 const equipments = require("./typedefs-resolvers/equipments");
 const supplies = require("./typedefs-resolvers/supplies");
 
-const typeDefs = [queries, mutations, equipments.typeDefs, supplies.typeDefs];
+const typeDefs = [queries, mutations, enums, equipments.typeDefs, supplies.typeDefs];
 
 const resolvers = [equipments.resolvers, supplies.resolvers];
 
--- typedefs-resolvers/_queries.js
+++ typedefs-resolvers/_queries.js
@@ -3,8 +3,8 @@ const { gql } = require("apollo-server");
 const typeDefs = gql`
     type Query {
         equipments: [Equipment]
+        equipmentAdvs: [EquipmentAdv]
         supplies: [Supply]
     }
 `;
-
 module.exports = typeDefs;
