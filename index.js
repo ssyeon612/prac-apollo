71aa2debaba725e62282f82593ccd1d94164e0ed
ssyeon612 <ssyeon612@gmai.com>
2024-04-04 23:21:28 +0900

    feat: union 타입 추가

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const { ApolloServer } = require("apollo-server");
-const _ = require("lodash");
 
 const queries = require("./typedefs-resolvers/_queries");
 const mutations = require("./typedefs-resolvers/_mutations");
 const enums = require("./typedefs-resolvers/_enums");
 const equipments = require("./typedefs-resolvers/equipments");
+const softwares = require("./typedefs-resolvers/softwares");
 const supplies = require("./typedefs-resolvers/supplies");
+const givens = require("./typedefs-resolvers/givens");
 
-const typeDefs = [queries, mutations, enums, equipments.typeDefs, supplies.typeDefs];
+const typeDefs = [queries, mutations, enums, equipments.typeDefs, softwares.typeDefs, supplies.typeDefs, givens.typeDefs];
 
-const resolvers = [equipments.resolvers, supplies.resolvers];
+const resolvers = [equipments.resolvers, softwares.resolvers, supplies.resolvers, givens.resolvers];
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
